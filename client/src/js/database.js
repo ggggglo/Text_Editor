fec734f0e606cdc6624c7f703a9a33d77a11a25e
Gloriana Gonzalez <glogonzalez@Glorianas-MacBook-Pro.local>
2022-12-14 11:44:04 -0600

    updated readme

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,23 +12,38 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// add logic to a method that accepts some content and adds it to the database.. LOOK AT ACT 24 JS/DATABASE.JS
 export const putDb = async (content) => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
+  console.log('PUT to database');
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  // Use the .get() method to get a piece of data from the database based on the id.
   const request = store.put({ jate: content });
+  // Get confirmation of the request.
   const result = await request;
-  console.log("Data saved to the database", result);
+    console.log('🚀 - data saved to the database', result.value);
 };
-// TODO: Add logic for a method that gets all the content from the database
+
+// add logic for a method that gets all the content from the database.. LOOK AT ACT 24 JS/DATABASE.JS
+// Export a function we will use to GET from the database.
 export const getDb = async () => {
-  const jateDB = await openDB("jate", 1);
-  const tx = jateDB.transaction("jate", "readonly");
-  const store = tx.objectStore("jate");
+  console.error('GET from database!');
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readonly');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  // Use the .get() method to get a piece of data from the database based on the id.
   const request = store.getAll();
+  // Get confirmation of the request.
   const result = await request;
-  console.log(result);
+    console.log('🚀 - data saved to database', result.value)
+  return result.value
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,20 +15,23 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      //html plugin
       new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "JATE",
+        template: './index.html',
+        title: 'JATE',
       }),
+      //TODO add inject manifest, service worker
       new InjectManifest({
-        swSrc: "./src-sw.js",
-        swDest: "src-sw.js",
-      }),
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }), 
+      //TODO add webpack pwa
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
         name: "Just Another Text Editor",
-        short_name: "JATE",
-        description: "Text Editor with offline capabilities using IndexedDB",
+        short_name: 'JATE',
+        description: "Text editor with offline capabilities",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
@@ -48,6 +48,7 @@ module.exports = () => {
 
     module: {
       rules: [
+        //Add CSS loaders and babel to webpack.
         {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
@@ -56,18 +57,14 @@ module.exports = () => {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/plugin-proposal-object-rest-spread",
-                "@babel/transform-runtime",
-              ],
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime']
             },
           },
-        }
-        
+        },
       ],
     },
-  };
+  }
 };
